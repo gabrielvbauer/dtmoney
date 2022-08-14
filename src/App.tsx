11bf1ab9cb1e8249831261fc7602e719570148f2
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-08-14 13:37:16 -0300

    feat: Started React Context API implementation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Modal from 'react-modal';
 import { GlobalStyle } from './styles/global';
 import { useState } from 'react';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionContext } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -19,7 +20,7 @@ export function App() {
 	};
   
   return (
-    <>
+    <TransactionContext.Provider value={[]}>
       <Header
         onOpenNewTransactionModal={handleOpenNewTransactionModal}
       />
@@ -29,6 +30,6 @@ export function App() {
         onRequestClose={handleCloneNewTransactionModalOpen}
       />
       <GlobalStyle />
-    </>
+    </TransactionContext.Provider>
   );
 }
