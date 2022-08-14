3a54b02b219d5310f267b4dc2e90532b7cff7825
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-08-14 14:34:53 -0300

    feat: Moving Transaction creation to TransactionContext file

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -14,7 +14,14 @@ interface TransactionProviderProps {
   children: ReactNode;
 }
 
-export const TransactionContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
+export const TransactionContext = createContext<TransactionsContextData>({} as TransactionsContextData);
 
 export function TransactionsProvider({children}: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
@@ -24,8 +31,12 @@ export function TransactionsProvider({children}: TransactionProviderProps) {
 			.then(response => setTransactions(response.data.transactions))
 	}, [])
 
+  function createTransaction(transaction: TransactionInput) {
+    api.post('/transactions', transaction);
+  }
+
   return (
-    <TransactionContext.Provider value={transactions}>
+    <TransactionContext.Provider value={{transactions, createTransaction}}>
       {children}
     </TransactionContext.Provider>
   )
