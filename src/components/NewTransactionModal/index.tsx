655fb973165176d5e8151b0e4cf84809db0b6046
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-08-14 14:42:07 -0300

    feat: New transaction insertion and listing

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -17,19 +17,26 @@ interface NewTransactionModalProps {
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
-  const [type, setType] = useState('');
+  const [type, setType] = useState('deposit');
   const [category, setCategory] = useState('');
   const {createTransaction} = useContext(TransactionContext)
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       type,
       category
     })
+
+    setTitle('')
+    setAmount(0)
+    setType('deposit')
+    setCategory('')
+
+    onRequestClose();
   }
 
   return (
