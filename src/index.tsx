8c81ed0b4b7290a73d93d39e0bc1c6d518895b91
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-08-14 09:30:10 -0300

    feat: Listing transactions from api and seeding mocked date in api

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-06 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Mensalidade faculdade',
+          type: 'withdraw',
+          category: 'Estudos',
+          amount: 1400,
+          createdAt: new Date('2021-02-10 14:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
